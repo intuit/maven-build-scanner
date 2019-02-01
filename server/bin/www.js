6bc994a0847052258f122a8fad03a1a36e1839e0
Alex Collins <alex_collins@intuit.com>
2019-02-01 11:46:29 +0000

    Fixes NPM warnings

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,57 +1,32 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
 var app = require("../app");
 var debug = require("debug")("tmp:server");
 var http = require("http");
 
-/**
- * Get port from environment and store in Express.
- */
-
 var port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
-/**
- * Create HTTP server.
- */
-
 var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on("error", onError);
 server.on("listening", onListening);
 
-/**
- * Normalize a port into a number, string, or false.
- */
 
 function normalizePort(val) {
 	var port = parseInt(val, 10);
 
 	if (isNaN(port)) {
-		// named pipe
 		return val;
 	}
 
 	if (port >= 0) {
-		// port number
 		return port;
 	}
 
 	return false;
 }
-
-/**
- * Event listener for HTTP server "error" event.
- */
 
 function onError(error) {
 	if (error.syscall !== "listen") {
@@ -76,10 +51,6 @@ function onError(error) {
 		throw error;
 	}
 }
-
-/**
- * Event listener for HTTP server "listening" event.
- */
 
 function onListening() {
 	var addr = server.address();
