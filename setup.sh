183d98fc553e8a61a0fce447d04d93be342f23d5
Marc Buchser <buero@marcbuchser.ch>
2020-09-15 13:16:37 +0200

    setup sh modified

--- setup.sh
+++ setup.sh
@@ -7,29 +7,20 @@ status() {
     echo "$(tput bold)>>> $1 <<<$(tput sgr0)"
 }
 
-status "1/5 Starting local Mongo container"
-[ -e db ] || mkdir db
-[ "$(docker ps -q -f name=build_scan_db)" = '' ] && docker run -d -v $(pwd)/db:/data/db -P -p 27017:27017 --rm --name build_scan_db mongo
+#status "1/5 Starting local Mongo container"
+#[ -e db ] || mkdir db
+#[ "$(docker ps -q -f name=build_scan_db)" = '' ] && docker run -d -v $(pwd)/db:/data/db -P -p 27017:27017 --rm --name build_scan_db mongo
+status "1/3 Starting local containers (web server mongo db)"
+docker-compose up --build -d
 
-
-status "2/5 Building Maven extension"
-mvn install -q
+status "2/3 Building Maven extension and copying to - maven.home/lib/ext"
+mvn package
+#-DskipTests
 
 cp -v target/maven-build-scanner-1.0.0-SNAPSHOT-jar-with-dependencies.jar $(mvn help:evaluate -Dexpression=maven.home -DforceStdout -q)/lib/ext/
 
-cd server
-
-status "3/5 Building web server"
-npm install
-
-
-status "4/5 Starting web server"
-npm start &
-
-cd -
-
-status "5/5 Creating your first scan"
-env MAVEN_BUILD_SCANNER=1 mvn verify -q -DskipTests
+status "3/3 Creating your first scan"
+#env MAVEN_BUILD_SCANNER=1 mvn verify -q -DskipTests
 
 status "Ready"
 
